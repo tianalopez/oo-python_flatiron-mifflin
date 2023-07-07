cba7af6c0d9c814ae332c467aa70d5c937198b5d
Adam La Rosa <adam.larosa@flatironschool.com>
2023-07-07 07:15:24 -0700

    ipdb

--- debug.py
+++ debug.py
@@ -1,17 +1,15 @@
-from lib.employee import *
-from lib.manager import *
+from lib.employee import Employee
+from lib.manager import Manager
 
 # Test your code here
 
 # e.g.
-#   m1 = Manager( 'Mr. Levi', 'HR', 33 )
-#   e1 = Employee( 'Norris', 40000, m1 )
+# m1 = Manager( 'Mr. Levi', 'HR', 33 )
+# e1 = Employee( 'Norris', 40000, m1 )
 
 
 
 
 
-
-
-# do not remove
+# your code above!
 import ipdb; ipdb.set_trace()
